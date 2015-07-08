f2fb18338ed9927c19c223eae3e14e004d3d54e9
cherydactyl <cheryl.orosz@gmail.com>
2015-07-08 16:08:45 -0400

    Added null input handling
    detects no arguments condition and notifies user

--- ExpITCondLabReverse/Program.cs
+++ ExpITCondLabReverse/Program.cs
@@ -10,6 +10,21 @@ namespace ExpITCondLabReverse
     {
         static void Main(string[] args)
         {
+            //assume the input will be as arguments
+            //check for null input first
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Sorry, no arguments detected.");
+            }
+            else
+            {
+                //actual strings detected!
+                Console.WriteLine("Thank you for your input.");
+            }
+
+            Console.WriteLine();    //whitespace
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
 
         }
     }
