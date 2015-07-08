9ce5aecb15e997427c5ebe98400404b11d7843ef
cherydactyl <cheryl.orosz@gmail.com>
2015-07-08 16:20:59 -0400

    Reversal of one string
    takes advantage of the fact that strings are basically arrays of chars

--- ExpITCondLabReverse/Program.cs
+++ ExpITCondLabReverse/Program.cs
@@ -19,7 +19,14 @@ namespace ExpITCondLabReverse
             else
             {
                 //actual strings detected!
-                Console.WriteLine("Thank you for your input.");
+                //Console.WriteLine("Thank you for your input.");
+                //Console.WriteLine();
+
+                //reverse the first string, arg [0]
+                //first challenge
+                //taking advantage of the fact that strings are essentially character arrays
+                string reverseFirst = new string(args[0].ToCharArray().Reverse().ToArray());
+                Console.WriteLine("The reverse of the first string, '" + args[0] + "', is '" + reverseFirst + "'");
             }
 
             Console.WriteLine();    //whitespace
