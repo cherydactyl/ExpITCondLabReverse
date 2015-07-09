da91f6d45083d6529e0f40ccb0844611b6b58237
cherydactyl <cheryl.orosz@gmail.com>
2015-07-09 14:24:25 -0400

    More explanation to user
    Added additional information for the user, including an explanation of
    the assumption that input will be as command line arguments.  Added
    output showing the original input contents.

--- ExpITCondLabReverse/Program.cs
+++ ExpITCondLabReverse/Program.cs
@@ -11,6 +11,10 @@ namespace ExpITCondLabReverse
         static void Main(string[] args)
         {
             //assume the input will be as arguments
+            //let the user know about that assumption
+            Console.WriteLine("Project Reverse");
+            Console.WriteLine("FYI this program expects to receive one or more arguments from the command line.");
+
             //check for null input first
             if (args.Length < 1)
             {
@@ -22,8 +26,8 @@ namespace ExpITCondLabReverse
                 //Console.WriteLine("Thank you for your input.");
                 //Console.WriteLine();
 
-                //reverse the first string, arg [0]
                 //first challenge
+                //reverse the first string, arg [0]
                 //taking advantage of the fact that strings are essentially character arrays
                 string reverseFirst = new string(args[0].ToCharArray().Reverse().ToArray());
                 Console.WriteLine("The reverse of the first string, '" + args[0] + "', is '" + reverseFirst + "'");
@@ -32,23 +36,25 @@ namespace ExpITCondLabReverse
                 //Extra Credit
                 //Reverse and print all the characters in all the arguments (array of strings)
                 //first concatenate all the strings in the args array into one string, in regular order
-                string allTheStringsCombined = "";
-                for (int i = 0; i < args.Length; i++)
-                {
-                    allTheStringsCombined = allTheStringsCombined + args[i];
-                    //Console.WriteLine(allTheStringsCombined);         //for debugging
-                }
+                string allTheStringsCombined = "";      //initialize an empty string
 
+                Console.Write("Input: ");  //prepare to show input as received
+                foreach (string word in args)
+                {
+                    allTheStringsCombined = allTheStringsCombined + word;
+                    //Console.WriteLine(allTheStringsCombined);         //for debugging
+                    Console.Write(word + " ");  //show the input strings one by one
+                }
+                Console.WriteLine();    //write a new line after the input string
+                Console.WriteLine();    //extra whitespace for readability
                 //and now reverse it, just like above
                 string reverseAll = new string(allTheStringsCombined.ToCharArray().Reverse().ToArray());
                 Console.WriteLine("When the full combined string '" + allTheStringsCombined + "' is reversed,");
-                Console.WriteLine("The result is '" + reverseAll + "'");
+                Console.WriteLine("the result is '" + reverseAll + "'");
             }
-
             Console.WriteLine();    //whitespace
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
-
         }
     }
 }
